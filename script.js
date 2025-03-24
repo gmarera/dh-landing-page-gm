ddf6bea13f3eb3eaeebb04a94ac1c199bfa16948
Germán <gmarera@gmail.com>
2025-03-24 03:09:58 -0300

    Esta rama y commit tienen agregado Java

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('#menu-icon');
+const navList = document.querySelector('.navlist');
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');   
+    navList.classList.toggle('navlist-open');
+}
+
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    delay: 450,
+    reset: true
+})
+
+sr.reveal('.hero-text', {delay: 200, origin: 'top'}) 
+sr.reveal('.hero-img', {delay: 450, origin: 'left'}) 
+sr.reveal('.icons', {delay: 300, origin: 'bottom'}) 
+sr.reveal('.scroll-down', {delay: 450, origin: 'right'}) 
